9cad981c705d59cad3889b1f25575a41bb431114
Sebastian <sebastianpatane@gmail.com>
2013-08-22 17:39:22 -0400

    cleaned up files, added kara ross luxury line current home page

---
+++ KaraRoss_45_files/nav.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+
+$('ul.subcateg').hide();//slidedown menu
+$('ul.submenu').hide();//slidedown menu
+$('ul.subsubmenu').hide();//slidedown menu
+
+var touchdevice = is_touch_device();
+
+  function is_touch_device() {
+    return !!('ontouchstart' in window) ? 1 : 0;
+  }
+
+$('ul.navmain > li').click( function () {
+
+if(touchdevice === 1) {
+if($(this).has("ul.submenu")){
+$(this).find("ul.submenu").addClass("move-left").show();
+$(this).addClass("move-top");
+$("ul.navmain").addClass("move-left");
+$("#res-go-back").show();
+$(".filterbar").hide();
+}
+
+}
+
+});
+
+
+$("#res-go-back img").click( function() {
+$("ul.navmain").removeClass("move-left");
+$("ul.submenu").removeClass("move-left").hide();	
+$('ul.navmain > li').removeClass("move-top");
+$("#res-go-back").hide();
+});
+
+$("#res-expand-drop img").click(function() {
+
+if($("ul.navmain").hasClass("hide")) {
+$(".navmayfix").show();
+$("video").hide();
+$("ul.navmain").removeClass("hide");
+} else {
+$(".navmayfix").hide();
+$("video").show();
+$("ul.navmain").addClass("hide");
+$("ul.navmain").removeClass("move-left");
+$("ul.submenu").removeClass("move-left").hide();	
+$('ul.navmain > li').removeClass("move-top");
+$("#res-go-back").hide();
+$(".filterbar").show();
+}
+
+});	
+
+});
---
+++ KaraRoss_45_files/persistentnav.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+
+// ANUJ -- adding responsive restraint for hover
+// SEBASTIAN -- edited so that it only triggers on touch devices
+
+  var touchdevice = is_touch_device();
+
+  function is_touch_device() {
+    return !!('ontouchstart' in window) ? 1 : 0;
+  }
+  
+  
+if(touchdevice === 1) {
+//menu code
+$('.navmain li').hover(
+
+function () {
+$(this).find('div').css('z-index','999999').end().find('ul.submenu').css('z-index','999999').end();
+$(this).css('z-index','10006');
+$('ul.submenu', this).stop(true, true).slideDown(200);
+},
+
+function () {
+$(this).find('div').css('z-index','10003').end().find('ul.submenu').css('z-index','10003').end();
+$(this).css('z-index','10003');
+$('ul.submenu', this).stop(true, true).slideUp(200);
+
+});
+} else {
+$('.navmain li').hover(
+			
+			function () {
+				$(this).find('div').css('z-index','10006').end().find('ul.submenu').css('z-index','10006').end();
+				$(this).css('z-index','10006');
+				$('ul.submenu', this).stop(true, true).slideDown(200);
+			},
+			
+			function () {
+				$(this).find('div').css('z-index','10003').end().find('ul.submenu').css('z-index','10003').end();
+					$(this).css('z-index','10003');
+				$('ul.submenu', this).stop(true, true).slideUp(200);  
+				         
+			});
+$('.navmain li .submenu li a').hover(
+	function() {
+		if($(this).children(":first").hasClass("subsubmenu"))
+		{
+			$(this).find('ul.subsubmenu').css('z-index','10003').end();
+			$('ul.subsubmenu', this).stop(true, true).slideDown(200);
+		
+		}
+	},
+	function() {
+		$(this).find('ul.subsubmenu').css('z-index','10003').end();
+		$('ul.subsubmenu', this).stop(true, true).slideUp(200);
+	
+	}
+);
+
+
+}
+ });
+ 
